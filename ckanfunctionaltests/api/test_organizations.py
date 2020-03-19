e93c6151e2f9d4d0e36916f2061ea08640c18e05
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-03-19 12:09:00 +0000

    test_organization_show: check same result can be obtained through uuid-based request

--- ckanfunctionaltests/api/test_organizations.py
+++ ckanfunctionaltests/api/test_organizations.py
@@ -19,7 +19,15 @@ def test_organization_show_404(base_url, rsession):
 def test_organization_show(base_url, rsession, random_org_slug):
     response = rsession.get(f"{base_url}/action/organization_show?id={random_org_slug}")
     assert response.status_code == 200
-    validate_against_schema(response.json(), "organization_show.schema.json")
 
-    assert response.json()["success"] is True
+    rj = response.json()
+    validate_against_schema(rj, "organization_show.schema.json")
+
+    assert rj["success"] is True
+    assert rj["result"]["name"] == random_org_slug
+
+    # we should be able to look up this same organization by its uuid and get an identical response
+    uuid_response = rsession.get(f"{base_url}/action/organization_show?id={rj['result']['id']}")
+    assert uuid_response.status_code == 200
+    assert uuid_response.json() == rj
 
