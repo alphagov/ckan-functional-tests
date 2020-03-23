e04b0f4ccb61cd7637d9af85250e41e62e4488e0
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-03-23 15:32:13 +0000

    add tests of package_search endpoint

--- ckanfunctionaltests/api/conftest.py
+++ ckanfunctionaltests/api/conftest.py
@@ -1,4 +1,5 @@
 from random import Random
+import re
 
 import pytest
 import requests
@@ -6,6 +7,9 @@ import requests
 
 # we will want to be able to seed this at some point
 _random = Random()
+
+
+_uuid_re = re.compile(r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", re.I)
 
 
 @pytest.fixture()
@@ -29,6 +33,23 @@ def random_org_slug(base_url, rsession):
 
 @pytest.fixture()
 def random_pkg_slug(base_url, rsession):
-    response = rsession.get(f"{base_url}/action/package_list")
+    # make do with one of the first 200 because the full list is big & slow
+    response = rsession.get(f"{base_url}/action/package_list?limit=200")
     assert response.status_code == 200
-    return _random.choice(response.json()["result"])
+
+    suitable_names = tuple(
+        name for name in response.json()["result"] if not _uuid_re.fullmatch(name)
+    )
+
+    if not suitable_names:
+        raise ValueError("No suitable package slugs found")
+
+    return _random.choice(suitable_names)
+
+
+@pytest.fixture()
+def random_pkg(base_url, rsession, random_pkg_slug):
+    response = rsession.get(f"{base_url}/action/package_show?id={random_pkg_slug}")
+    assert response.status_code == 200
+
+    return response.json()["result"]
