a2d4f3aa5f95458b62048c636bee3b90addb2af3
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-04-03 17:45:39 +0100

    test endpoints also accessible under a api-versioned path, adapt to differences
    using parametrized fixtures to achieve this. there are a few differences
    between the parameters expected/supported by the unversioned and `/3/`
    endpoints. in some cases a feature/syntax is entirely unsupported - in
    these cases, skip that particular parameter combination.

--- ckanfunctionaltests/api/test_organizations.py
+++ ckanfunctionaltests/api/test_organizations.py
@@ -1,23 +1,23 @@
 from ckanfunctionaltests.api import validate_against_schema
 
 
-def test_organization_list(base_url, rsession):
-    response = rsession.get(f"{base_url}/action/organization_list")
+def test_organization_list(base_url_3, rsession):
+    response = rsession.get(f"{base_url_3}/action/organization_list")
     assert response.status_code == 200
     validate_against_schema(response.json(), "organization_list")
 
     assert response.json()["success"] is True
 
 
-def test_organization_show_404(base_url, rsession):
-    response = rsession.get(f"{base_url}/action/organization_show?id=number-three-group")
+def test_organization_show_404(base_url_3, rsession):
+    response = rsession.get(f"{base_url_3}/action/organization_show?id=number-three-group")
     assert response.status_code == 404
 
     assert response.json()["success"] is False
 
 
-def test_organization_show(subtests, base_url, rsession, random_org_slug):
-    response = rsession.get(f"{base_url}/action/organization_show?id={random_org_slug}")
+def test_organization_show(subtests, base_url_3, rsession, random_org_slug):
+    response = rsession.get(f"{base_url_3}/action/organization_show?id={random_org_slug}")
     assert response.status_code == 200
     rj = response.json()
 
@@ -29,6 +29,6 @@ def test_organization_show(subtests, base_url, rsession, random_org_slug):
 
     with subtests.test("uuid lookup consistency"):
         # we should be able to look up this same organization by its uuid and get an identical response
-        uuid_response = rsession.get(f"{base_url}/action/organization_show?id={rj['result']['id']}")
+        uuid_response = rsession.get(f"{base_url_3}/action/organization_show?id={rj['result']['id']}")
         assert uuid_response.status_code == 200
         assert uuid_response.json() == rj
