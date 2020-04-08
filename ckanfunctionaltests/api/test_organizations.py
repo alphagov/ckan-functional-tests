c56475a09a1c4864cfc9c21f4d4d68a83858d03d
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-04-08 13:05:28 +0100

    organization_list tests: add tests for extended response options

--- ckanfunctionaltests/api/test_organizations.py
+++ ckanfunctionaltests/api/test_organizations.py
@@ -1,12 +1,74 @@
+from dmtestutils.comparisons import AnySupersetOf
+
+
 from ckanfunctionaltests.api import validate_against_schema
 
 
 def test_organization_list(base_url_3, rsession):
     response = rsession.get(f"{base_url_3}/action/organization_list")
     assert response.status_code == 200
-    validate_against_schema(response.json(), "organization_list")
+    rj = response.json()
+    validate_against_schema(rj, "organization_list")
 
-    assert response.json()["success"] is True
+    assert rj["success"] is True
+    # assert this is the correct variant of the response
+    assert isinstance(rj["result"][0], str)
+
+
+def test_organization_list_all_fields(subtests, base_url_3, rsession):
+    response = rsession.get(f"{base_url_3}/action/organization_list?all_fields=1&limit=5")
+    assert response.status_code == 200
+    rj = response.json()
+
+    with subtests.test("response validity"):
+        validate_against_schema(rj, "organization_list")
+        assert rj["success"] is True
+        # assert this is the correct variant of the response schema
+        assert isinstance(rj["result"][0], dict)
+
+    with subtests.test("consistency with organization_show"):
+        os_response = rsession.get(f"{base_url_3}/action/organization_show?id={rj['result'][0]['id']}")
+        assert os_response.status_code == 200
+
+        assert os_response.json()["result"] == AnySupersetOf(rj['result'][0])
+
+
+def test_organization_list_all_fields_inc_optional(subtests, base_url_3, rsession):
+    response = rsession.get(
+        f"{base_url_3}/action/organization_list?all_fields=1&include_extras=1&include_tags=1"
+        "&include_groups=1&limit=5"
+    )
+    assert response.status_code == 200
+    rj = response.json()
+
+    with subtests.test("response validity"):
+        validate_against_schema(rj, "organization_list")
+        assert rj["success"] is True
+        # assert this is the correct variant of the response schema
+        assert isinstance(rj["result"][0], dict)
+        assert "extras" in rj["result"][0]
+        assert "tags" in rj["result"][0]
+        assert "groups" in rj["result"][0]
+
+    with subtests.test("consistency with organization_show"):
+        os_response = rsession.get(f"{base_url_3}/action/organization_show?id={rj['result'][0]['id']}")
+        assert os_response.status_code == 200
+
+        assert os_response.json()["result"] == AnySupersetOf(rj['result'][0])
+
+
+def test_organization_list_all_fields_inc_users_no_effect(subtests, base_url_3, rsession):
+    incusers_response = rsession.get(
+        f"{base_url_3}/action/organization_list?all_fields=1&include_users=1&limit=5"
+    )
+    assert incusers_response.status_code == 200
+
+    excusers_response = rsession.get(
+        f"{base_url_3}/action/organization_list?all_fields=1&include_users=0&limit=5"
+    )
+    assert excusers_response.status_code == 200
+
+    assert incusers_response.json() == excusers_response.json()
 
 
 def test_organization_show_404(base_url_3, rsession):
